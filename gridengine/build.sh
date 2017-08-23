fcf24069eb9488fc101ee27577851dce513b69e7
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-08-23 11:13:29 +0200

    simplified queue

--- gridengine/build.sh
+++ gridengine/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --rm --add-host "gridengine:127.0.0.1" --add-host "wps:127.0.0.1" -t birdhouse/gridengine .
+docker build --rm --add-host "gridengine:127.0.0.1" --add-host "wps:127.0.0.1" --add-host "gridengine-wps:127.0.0.1" -t birdhouse/gridengine .
